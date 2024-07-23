b82a4c2dc8210d3b318f20f469e8afe82d84a4c5
maxirosanda <maxi_rosanda@hotmail.com>
2024-07-23 19:58:22 -0300

    clase 3 primera parte

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import {StyleSheet, View, TextInput, Pressable,Text} from 'react-native'
+import ButtonPrimary from './src/components/ButtonPrimary'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+
+    return(
+          <View style={styles.container}>
+            <View style={styles.containerInput}>
+              <TextInput style={styles.input} placeholder='Ingrese una tarea'/>
+              <ButtonPrimary text="Agregar"/>
+            </View>
+          </View>
+    )
 }
 
+export default App
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    marginTop:30,
+    flex:1
   },
-});
+  containerInput:{
+    flexDirection:"row",
+    padding:10
+  },
+  input:{
+    borderColor:"black",
+    borderWidth:1,
+    padding:5,
+    paddingStart:20,
+    flex:2,
+    margin:10,
+    borderRadius:5
+  }
+})
---
+++ src/components/ButtonPrimary.js
@@ -0,0 +1,28 @@
+import { StyleSheet,Pressable,Text } from 'react-native'
+
+const ButtonPrimary = ({text}) => {
+  return (
+            <Pressable style={styles.button}>
+                <Text style={styles.textButton}>{text}</Text>
+            </Pressable>
+  )
+}
+
+export default ButtonPrimary
+
+const styles = StyleSheet.create({
+    button:{
+        padding:20,
+        backgroundColor:"red",
+        flex:1,
+        margin:10,
+        alignItems:"center",
+        justifyContent:"center",
+        borderRadius:5
+      },
+      textButton:{
+        color:"white",
+        fontSize:18,
+        fontWeight:"bold"
+      }
+})
